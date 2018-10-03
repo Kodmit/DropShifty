68d65e5b70a3af02a98c4088a8082a54b99ef419
julien <julien.moratalla@gmail.com>
2018-10-03 11:01:00 +0200

    add react for front

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import {Route, BrowserRouter} from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+            <Route path="/" component={Home} exact/>
+            <Route path="/contact" component={Contact}/>
+        </div>
+      </BrowserRouter>
+    );
+  };
+};
+
+export default App;
---
+++ front/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <p>This is the homepage</p>
+        </div>
+    );
+};
+
+export default Home;
+
