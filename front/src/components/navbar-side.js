9ced0dc90ff9c2212401820c08ac3190af5d0b7d
julien <julien.moratalla@gmail.com>
2018-10-24 17:43:53 +0200

    frontin process

--- front/src/components/navbar-side.js
+++ front/src/components/navbar-side.js
@@ -5,12 +5,12 @@ const NavbarSide = () => {
     return (
         <nav className="navbar-side">
             <ul>
-                <li><a href="#"><img src="images/icons/line-chart.svg"></img></a></li>
-                <li><a href="#"><img src="images/icons/smartphone.svg"></img></a></li>
-                <li><a href="#"><img src="images/icons/shopping-bag.svg"></img></a></li>
-                <li><a href="#"><img src="images/icons/notifications-button.svg"></img></a></li>
-                <li><a href="#"><img src="images/icons/web.svg"></img></a></li>
-                <li><a href="#"><img src="images/icons/settings.svg"></img></a></li>
+                <li className="dashboard"><a href="#"><img src="images/icons/line-chart.svg"></img></a></li>
+                <li className="smartphones"><a href="#"><img src="images/icons/smartphone.svg"></img></a></li>
+                <li className="shopping"><a href="#"><img src="images/icons/shopping-bag.svg"></img></a></li>
+                <li className="notifications"><a href="#"><img src="images/icons/notifications-button.svg"></img></a></li>
+                <li className="website"><a href="#"><img src="images/icons/web.svg"></img></a></li>
+                <li className="settings"><a href="#"><img src="images/icons/settings.svg"></img></a></li>
             </ul>
         </nav>
     );
