1706663163acea4bee99aebbdb0c5fc5f40a6da9
julien <julien.moratalla@gmail.com>
2018-10-24 20:18:58 +0200

    frontin process

--- front/src/components/navbar-side.js
+++ front/src/components/navbar-side.js
@@ -4,14 +4,69 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const NavbarSide = () => {
     return (
         <nav className="navbar-side">
-            <ul>
-                <li className="dashboard"><a href="#"><img src="images/icons/line-chart.svg"></img></a></li>
-                <li className="smartphones"><a href="#"><img src="images/icons/smartphone.svg"></img></a></li>
-                <li className="shopping"><a href="#"><img src="images/icons/shopping-bag.svg"></img></a></li>
-                <li className="notifications"><a href="#"><img src="images/icons/notifications-button.svg"></img></a></li>
-                <li className="website"><a href="#"><img src="images/icons/web.svg"></img></a></li>
-                <li className="settings"><a href="#"><img src="images/icons/settings.svg"></img></a></li>
-            </ul>
+
+            <div className="row  mt-1">
+                <div className="col-sm-2">
+                    <img className="icons-nav" src="images/icons/line-chart.svg"></img>
+                </div>
+                <div className="col-sm-10">
+                    <a href="">Dashboard</a>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-sm-2">
+                    <img className="icons-nav" src="images/icons/smartphone.svg"></img>
+                </div>
+                <div className="col-sm-10">
+                    <a href="">Commandes</a>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-sm-2">
+                    <img className="icons-nav" src="images/icons/shopping-bag.svg"></img>
+                </div>
+                <div className="col-sm-10">
+                    <a href="">Importer produit</a>
+                </div>
+                <div className="col-sm-2"></div>
+                <div className="col-sm-10">
+                    <a href="">Produits importés</a>
+                </div>
+                <div className="col-sm-2"></div>
+                <div className="col-sm-10">
+                    <a href="">Mes produits</a>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-sm-2">
+                    <img className="icons-nav" src="images/icons/notification.svg"></img>
+                </div>
+                <div className="col-sm-10">
+                    <a href="">Notifications</a>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-sm-2">
+                    <img className="icons-nav" src="images/icons/web.svg"></img>
+                </div>
+                <div className="col-sm-10">
+                    <a href="">My website</a>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col-sm-2">
+                    <img className="icons-nav" src="images/icons/settings.svg"></img>
+                </div>
+                <div className="col-sm-10">
+                    <a href="">Parametres</a>
+                </div>
+            </div>
+
         </nav>
     );
 };
