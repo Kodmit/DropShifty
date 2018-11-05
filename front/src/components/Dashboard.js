13dfaebf5199b4c457c21d88962ea0412d4a2a7c
julien <julien.moratalla@gmail.com>
2018-11-05 22:00:32 +0100

    dashboard view in process

--- front/src/components/Dashboard.js
+++ front/src/components/Dashboard.js
@@ -16,9 +16,43 @@ const Dashboard = () => {
                         </div>  
                     </div>
                     <div className="col-sm-4">
-                        <div className="overview"></div>
+                        <div className="overview">
+                            <p className="p-2">Vue d'ensemble</p>
+
+                            <div className="separator"></div>
+
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-sm-2">
+                                        <p>Ventes</p>
+                                    </div>
+                                    <div className="col-sm-8"></div>
+                                    <div className="col-sm-2">
+                                        <p>19.90 EUR</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-sm-2">
+                                        <p>Commandes</p>
+                                    </div>
+                                    <div className="col-sm-8"></div>
+                                    <div className="col-sm-2">
+                                        <p>3</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="separator"></div>
+
+                            
+
+                        </div>
                     </div>
-                </div> 
+                </div>
+
+                <div className="mt-5"></div>
+
+                <h3>Produits les mieux vendus</h3>  
                 
             </div> 
         </div>
