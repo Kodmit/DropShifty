a69486303d9e2c666d07f2e1538b99d1199f0aed
julien <julien.moratalla@gmail.com>
2018-11-13 16:31:30 +0100

    order view in process

--- front/src/components/Orders.js
+++ front/src/components/Orders.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/orders.scss';
+import '../styles/app.scss';
 
 const Orders = () => {
     return (
         <div>
-            <div className="container mt-3">
-                <p>Orders view</p>   
+            <div className="container mt-4">
+                <h3>Liste des commandes</h3>
+
+                <div className="row mt-5">
+                    <div className="col-sm-12">
+                        <div className="order-box">
+                            <div className="container text-order-box">
+                                <p>Commande</p>
+                                <p className="nb-order">#1001</p>
+                                <p>10/12/2018</p>
+                                <p>Client : Roger Horloger</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div> 
         </div>
     );
