9a81f9bbf01d8626a3d0e737578ce60896568c8b
julien <julien.moratalla@gmail.com>
2019-02-08 21:55:19 +0100

    finish auth

--- front/src/App.js
+++ front/src/App.js
@@ -16,69 +16,96 @@ import Register from './components/Register';
 import {Route, BrowserRouter, PrivateRoute} from "react-router-dom";
 import $ from "jquery";
 import { request, GraphQLClient } from 'graphql-request';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from "apollo-boost";
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 
 
+const link = createHttpLink({
+    uri: 'http://localhost:8000',
+    withCredentials: true
+  });
+  
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link,
+  });
+/*
+  client
+  .query({
+    query: gql`
+        {
+            CheckIfConnected
+        }`
+  })
+  .then(result => console.log(result.data)); */
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            connected: 'true',
-        }
+    state = {
+        connected: false
     }
 
     componentDidMount() {
-        this.checkIfConnected();
-        if (this.checkIfConnected() == 'true') {
-            console.log("checkifconnected egal true");
-        }
-    }
 
-    checkIfConnected() {
-        const client = new GraphQLClient('http://localhost:8000', {
-            headers: {
-                Authorization: 'Bearer my-jwt-token',
-            },
+        let self = this;
+        let data = "{\"query\":\"{\\n\\t " + 'CheckIfConnected' + " \\n}\"}";
+        let xhr = new XMLHttpRequest();
+
+        xhr.withCredentials = true;
+        xhr.open("POST", "http://localhost:8000/");
+        xhr.setRequestHeader("content-type", "application/json");
+        xhr.send(data);
+
+        console.log(data)
+        
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+            let object = JSON.parse(this.response);
+            let res = object.data['CheckIfConnected'];
+
+            self.setState({
+                connected: res
+                });
+            }
         });
-
-        const query = `{
-          CheckIfConnected
-        }`;
-
-        client.request(query).then(data => console.log(data));
     }
 
 
     render() {
-        if (this.state.connected == 'true') {
+
+        if (this.state.connected == true) {
+            return (
+                <ApolloProvider client={client}>
+                    <BrowserRouter>
+                        <div>
+                            <div className="grid-container">
+                                <Header/>
+                                <NavbarSide/>
+                                <Route path="/" component={Dashboard} exact/>
+                                <Route path="/orders" component={Orders}/>
+                                <Route path="/import" component={ImportProducts}/>
+                                <Route path="/imported" component={ImportedProducts}/>
+                                <Route path="/products" component={MyProducts}/>
+                                <Route path="/notifications" component={Notifications}/>
+                                <Route path="/parameters" component={Parameters}/>
+                            </div>
+                        </div>
+                    </BrowserRouter>
+                </ApolloProvider>
+            );
+        } else {
             return (
                 <BrowserRouter>
                     <div>
                         <Route path="/login" component={Login} />
-                        <Route path="/register" component={Register} />
-                        <div className="grid-container">
-                            <Header/>
-                            <NavbarSide/>
-                            <Route path="/" component={Dashboard} exact/>
-                            <Route path="/orders" component={Orders}/>
-                            <Route path="/import" component={ImportProducts}/>
-                            <Route path="/imported" component={ImportedProducts}/>
-                            <Route path="/products" component={MyProducts}/>
-                            <Route path="/notifications" component={Notifications}/>
-                            <Route path="/parameters" component={Parameters}/>
-                        </div>
                     </div>
                 </BrowserRouter>
-            );
-        } else {
-            return (
-                <div>
-                    <p>user not connected</p>
-                </div>
+
             )
         }
-
   };
 }
 
--- front/src/components/UserForm.js
+++ front/src/components/UserForm.js
@@ -6,6 +6,32 @@ import axios from 'axios';
 import Alert from '../components/includes/alert/Alert';
 import {findDOMNode} from "react-dom";
 import $ from "jquery";
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from "apollo-boost";
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import './includes/query';
+import gql from 'graphql-tag';
+
+
+  const link = createHttpLink({
+    uri: 'http://localhost:8000',
+    withCredentials: true
+  });
+  
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link,
+  });
+
+  client
+  .query({
+    query: gql`
+        {
+            CheckIfConnected
+        }`
+  })
+  .then(result => console.log(result.data));
 
 
 class UserForm extends Component {
@@ -17,6 +43,35 @@ class UserForm extends Component {
             alert_type: ''
         }
     }
+
+    componentDidMount() {
+        console.log("last true");this.checkIfConnected();
+    }
+
+    checkIfConnected() {
+        /*
+        function ds_call(arg, handledata){
+
+            let data = "{\"query\":\"{\\n\\t " + arg + " \\n}\"}";
+            let xhr = new XMLHttpRequest();
+          
+            xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === this.DONE) {
+                let object = JSON.parse(this.response);
+                handledata(object.data[arg]);
+              }
+            });
+            xhr.withCredentials = true;
+            xhr.open("POST", "http://localhost:8000/");
+            xhr.setRequestHeader("content-type", "application/json");
+            xhr.send(data);
+          
+          }
+        
+          ds_call('CheckIfConnected', function(out) {
+              console.log(out);
+          }) */
+    }
     
     // Get datas from rest api using axios.
      submitLogin = (e) => {
@@ -25,15 +80,25 @@ class UserForm extends Component {
       const pass = e.target.elements.password.value;
 
       if (user && pass) {
+        axios.defaults.withCredentials = true;
         axios.post('http://localhost:8000/login?username='+ user +'&password='+ pass)
         .then((res) => {
           console.log(res.data);
           let response = res.data.response;
-            if (response == 'ok') {
+            console.log(response);
+            if (response == 'ok' && this.checkIfConnected() == true) {
               this.setState({alert_message: 'Connecté avec succes'});
               this.setState({alert_type: 'success'});
               window.location = '/';
-          } else {
+          }
+
+          if (response == 'already logged in') {
+            this.setState({alert_message: 'Connecté avec succes'});
+            this.setState({alert_type: 'success'});
+            window.location = '/';
+        } 
+          
+          else {
               this.setState({alert_message: 'Identifiant ou mot de passe inccorect'});
               this.setState({alert_type: 'danger'});
           }
@@ -52,28 +117,30 @@ class UserForm extends Component {
 
     render() {
         return (
-            <div className="login_view">
-                <div id="user_form" className="container">
-                    <img className="logo_drop mx-auto d-block" src="/images/logo-drop.png" alt="Logo dropshifty"/>
+            <ApolloProvider client={client}>
+                <div className="login_view">
+                    <div id="user_form" className="container">
+                        <img className="logo_drop mx-auto d-block" src="/images/logo-drop.png" alt="Logo dropshifty"/>
 
-                    <div className="mt-4">
-                        {this.state.alert_type == 'success'?<Alert type={this.state.alert_type} message={this.state.alert_message} />:null}
-                        {this.state.alert_type == 'danger'?<Alert type={this.state.alert_type} message={this.state.alert_message}/>:null}
-                        {this.state.alert_type == 'warning'?<Alert type={this.state.alert_type} message={this.state.alert_message}/>:null}
-                    </div>
-
-                    <form onSubmit={this.submitLogin}>
-                        <div className="form-group">
-                            <label htmlFor="username">Login</label>
-                            <input required="required" type="text" name="username" className="form-control" id="username" placeholder="Nom utilisateur" />
-                            <br/>
-                            <label htmlFor="username">Mot de passe</label>
-                            <input required="required" type="password" name="password" className="form-control" id="password" placeholder="Mot de passe" />
+                        <div className="mt-4">
+                            {this.state.alert_type == 'success'?<Alert type={this.state.alert_type} message={this.state.alert_message} />:null}
+                            {this.state.alert_type == 'danger'?<Alert type={this.state.alert_type} message={this.state.alert_message}/>:null}
+                            {this.state.alert_type == 'warning'?<Alert type={this.state.alert_type} message={this.state.alert_message}/>:null}
                         </div>
-                        <button onClick={this.loginBtn} type="submit" className="btn_register mt-3">Connexion</button>
-                    </form>
+
+                        <form onSubmit={this.submitLogin}>
+                            <div className="form-group">
+                                <label htmlFor="username">Login</label>
+                                <input required="required" type="text" name="username" className="form-control" id="username" placeholder="Nom utilisateur" />
+                                <br/>
+                                <label htmlFor="username">Mot de passe</label>
+                                <input required="required" type="password" name="password" className="form-control" id="password" placeholder="Mot de passe" />
+                            </div>
+                            <button onClick={this.loginBtn} type="submit" className="btn_register mt-3">Connexion</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            </ApolloProvider>
         );
     };
 
