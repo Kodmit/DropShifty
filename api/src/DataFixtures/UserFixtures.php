6976a3a9096597107c7d72eb64d062e16c4821be
julien <julien.moratalla@gmail.com>
2019-02-13 12:41:54 +0100

    fix conflicts on fixtures

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -24,14 +24,8 @@ class UserFixtures extends Fixture
         $user1->setUsername("admin");
         $user1->setPassword("$2y$13$.LsNIc4MoMb1D3XgZQ11cOiXVwApFWr7dGX56NcrKbiyyjmQfcKSS"); // -> password
         $user1->setEmail("contact@dropshifty.com");
-<<<<<<< HEAD
-        $user1->setApiKey('key');
-        $user1->setFirstname('toto');
-        $user1->setLastname('toto');
-=======
         $user1->setApiKey(bin2hex(random_bytes(255)));
         $user1->setUserKey($this->userKey);
->>>>>>> alex
 
         $manager->persist($user1);
         $manager->flush();
