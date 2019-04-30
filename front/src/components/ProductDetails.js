252a5749a502eae891a6a99dee00be24e108616b
julien93140 <julien.moratalla@gmail.com>
2019-04-30 16:51:07 +0200

    view product in process

--- front/src/components/ProductDetails.js
+++ front/src/components/ProductDetails.js
@@ -8,20 +8,67 @@ class ProductDetails extends Component {
 
     constructor(props) {
         super(props)
-        this.state = { }
+        this.state = {
+            productInfos: []
+        }
     }
 
     componentDidMount() {
+        let self = this;
+        let product_id = this.props.match.params.id;
 
+        console.log(product_id);
+        this.getProductDetails(product_id);
+    }
+
+    getProductDetails(product_id) {
+        this.ds_call("WC_GetProduct(id:" + product_id + ")");
+    }
+
+    ds_call(arg, handledata) {
+        let self = this;
+        let data = "{\"query\":\"{\\n\\t " + arg + " \\n}\"}";
+        let xhr = new XMLHttpRequest();
+
+            xhr.addEventListener("readystatechange", function () {
+
+            if (this.readyState === this.DONE) {
+                //console.log(this.response);
+                let object = JSON.parse(this.response);
+                let objectParsed = object.data.WC_GetProduct;
+
+                self.setState({
+                    productInfos: objectParsed
+                })
+
+                console.log(self.state.productInfos);
+                
+            }
+        });
+
+        xhr.withCredentials = true;
+        xhr.open("POST", "https://ds-api2.herokuapp.com/");
+        xhr.setRequestHeader("content-type", "application/json");
+        xhr.send(data);
     }
 
     render() {
+        this.items = this.state.productInfos.map((item, key) =>
+            <div key={item.id} className="container mt-4">
+                <div className="row">
+                    <div className="col-6">
+                        <img src={item.images[0].src} />
+                    </div>
+                    <div className="col-6">
+                        {item.name}
+                    </div>
+                </div>
+            </div>
+        );
+        
         return (
             <div className="main">
-                <div className="container mt-4">
-                    <h3>Product name</h3>
-                        {this.items}
-                </div>
+                {this.items}
             </div>
         );
     }
--- front/src/components/includes/NavbarSide.js
+++ front/src/components/includes/NavbarSide.js
@@ -9,7 +9,7 @@ const NavbarSide = () => {
 
             <div className="row navbar-content">
                 <div className="col-sm-2">
-                    <img className="icons-nav" src="images/icons/line-chart.svg" alt="graph"></img>
+                    <img className="icons-nav" src="./images/icons/line-chart.svg" alt="graph"></img>
                 </div>
                 <div className="col-sm-10">
                     <NavLink to={"/"} exact activeStyle={{color: "#33d5db"}}>Dashboard</NavLink>
@@ -18,7 +18,7 @@ const NavbarSide = () => {
 
             <div className="row mt-4">
                 <div className="col-sm-2">
-                    <img className="icons-nav" src="images/icons/smartphone.svg" alt="smartphone"></img>
+                    <img className="icons-nav" src="./images/icons/smartphone.svg" alt="smartphone"></img>
                 </div>
                 <div className="col-sm-10">
                     <NavLink to={"/orders"} exact activeStyle={{color: "#33d5db"}}>Commandes</NavLink>
@@ -27,7 +27,7 @@ const NavbarSide = () => {
 
             <div className="row mt-4">
                 <div className="col-sm-2">
-                    <img className="icons-nav" src="images/icons/shopping-bag.svg" alt="shopping"></img>
+                    <img className="icons-nav" src="./images/icons/shopping-bag.svg" alt="shopping"></img>
                 </div>
                 <div className="col-sm-10">
                     <NavLink to={"/import"} exact activeStyle={{color: "#33d5db"}}>Importer produit</NavLink>
@@ -44,7 +44,7 @@ const NavbarSide = () => {
 
             <div className="row mt-4">
                 <div className="col-sm-2">
-                    <img className="icons-nav" src="images/icons/notification.svg" alt="notifications"></img>
+                    <img className="icons-nav" src="./images/icons/notification.svg" alt="notifications"></img>
                 </div>
                 <div className="col-sm-10">
                     <NavLink to={"/notifications"} exact activeStyle={{color: "#33d5db"}}>Notifications</NavLink>
@@ -53,7 +53,7 @@ const NavbarSide = () => {
 
             <div className="row mt-4">
                 <div className="col-sm-2">
-                    <img className="icons-nav" src="images/icons/web.svg" alt="web"></img>
+                    <img className="icons-nav" src="./images/icons/web.svg" alt="web"></img>
                 </div>
                 <div className="col-sm-10">
                     <a href="http://wordpress.dev.dropshifty.com" target="_blank">Mon site</a>
@@ -62,7 +62,7 @@ const NavbarSide = () => {
 
             <div className="row mt-4">
                 <div className="col-sm-2">
-                    <img className="icons-nav" src="images/icons/settings.svg" alt="parametres"></img>
+                    <img className="icons-nav" src="./images/icons/settings.svg" alt="parametres"></img>
                 </div>
                 <div className="col-sm-10">
                     <NavLink to={"/parameters"} exact activeStyle={{color: "#33d5db"}}>Parametres</NavLink>
