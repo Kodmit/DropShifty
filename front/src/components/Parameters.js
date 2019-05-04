6b18239957125770893ef8f4009f00609c5733bd
julien93140 <julien.moratalla@gmail.com>
2019-05-04 10:34:35 +0200

    parameters in process

--- front/src/components/Parameters.js
+++ front/src/components/Parameters.js
@@ -15,7 +15,12 @@ class Parameters extends Component {
 
     submitParameters = (e) => {
         e.preventDefault();
-        const apiKey = e.target.elements.apikey.value;
+        const name = e.target.elements.shop_name.value;
+        const drescription = e.target.elements.shop_description.value;
+        const city = e.target.elements.shop_city.value;
+        const postalCode = e.target.elements.postal_code.value;
+        const address = e.target.elements.shop_address.value;
+        const shopUrl = e.target.elements.shop_url.value;
 
         client.mutate({
             mutation: gql`
@@ -26,24 +31,19 @@ class Parameters extends Component {
           }`,
           variables: { 
               "shop": {
-                "name": "Mon shop de test",
+                "name": name,
                 "categoryId": 2,
                 "countryId": 752,
-                "city": "Paris",
-                "url": "http://wordpress.dev.dropshifty.com",
-                "wcApiUrl": "http://wordpress.dev.dropshifty.com",
-                "postalCode": 75013,
-                "description": "Description du shop de test qui peut aussi être null",
-                "addressLine1": "6 rue de la meute",
+                "city": city,
+                "url": shopUrl,
+                "wcApiUrl": shopUrl,
+                "postalCode": postalCode,
+                "description": drescription,
+                "addressLine1": address,
                 "addressLine2": null,
                 "picturePath": null
             } },
         }).then(result => console.log(result.data));
-    };
-
-    parameterBtn = (e) => {
-        e.preventDefault();
-        console.log("click on param btn");
     };
 
     render() {
@@ -61,10 +61,36 @@ class Parameters extends Component {
                                 <div className="mt-3">
                                     <form onSubmit={this.submitParameters}>
                                         <div className="form-group">
-                                            <label htmlFor="apikey">Clé api</label>
-                                            <input required="required" type="text" name="apikey" className="_form-control" id="apikey" placeholder="Email"/>
+                                            <label htmlFor="shop_name">Nom de la boutique</label>
+                                            <input required="required" type="text" name="shop_name" className="_form-control" id="shop_name" placeholder="Nom de la boutique"/>
                                         </div>
-                                        <button onClick={this.parameterBtn} type="submit" className="btn-import mt-3">Enregistrer</button>
+
+                                        <div className="form-group">
+                                            <label htmlFor="shop_description">Description</label>
+                                            <input required="required" type="text" name="shop_description" className="_form-control" id="shop_description" placeholder="Description"/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="shop_city">Ville</label>
+                                            <input required="required" type="text" name="shop_city" className="_form-control" id="shop_city" placeholder="Ville de la boutique"/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="shop_address">Adresse</label>
+                                            <input required="required" type="text" name="shop_address" className="_form-control" id="shop_address" placeholder="Ville de la boutique"/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="postal_code">Code postal</label>
+                                            <input required="required" type="text" name="postal_code" className="_form-control" id="postal_code" placeholder="Code postal de la boutique"/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="shop_url">Url de la boutique</label>
+                                            <input required="required" type="text" name="shop_url" className="_form-control" id="shop_url" placeholder="Url de la boutique"/>
+                                        </div>
+
+                                        <input type="submit" className="btn-import mt-3" value="Enregistrer" />
                                     </form>
                                 </div>
                             </div>
