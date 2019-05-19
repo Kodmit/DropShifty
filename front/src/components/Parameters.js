884313f2eece27008ca04aec576ac9d3bdd412c4
julien93140 <julien.moratalla@gmail.com>
2019-05-19 14:35:20 +0200

    use axios for graphQL mutation

--- front/src/components/Parameters.js
+++ front/src/components/Parameters.js
@@ -6,6 +6,8 @@ import axios from "axios";
 import gql from 'graphql-tag';
 import { Mutation, withApollo } from "react-apollo";
 import ApolloClient from "apollo-boost";
+import { print } from 'graphql';
+
 
 const client = new ApolloClient({
     uri: "https://ds-api2.herokuapp.com/"
@@ -23,25 +25,34 @@ class Parameters extends Component {
         const address = e.target.elements.shop_address.value;
         const shopUrl = e.target.elements.shop_url.value;
 
-
-       //var data = '{\"query\":\"mutation NewShop($shop: ShopInput!) {\\n  NewShop(input: $shop) {\\n content\\n }\\n}\",\"variables\":{\"shop\":{\"name\":' + '\\\"' + name + '\\\"' + ',\"categoryId\":2,\"countryId\":752,\"city\":' + '\\\"' + city + '\\\"' + ',\"url\":' + '\\\"' + shopUrl + '\\\"' + ',\"wcApiUrl\":' + '\\\"' + shopUrl + '\\\"' + ',\"postalCode\":' + '\\\"' + postalCode + '\\\"' + ',\"description\":' + '\\\"' + description + '\\\"' + ',\"addressLine1\":' + '\\\"' + postalCode + '\\\"' + ',\"addressLine2\":null,\"picturePath\":null}},\"operationName\":\"NewShop\"}';
-
-       var data = "{\"query\":\"mutation NewShop($shop: ShopInput!) {\\n  NewShop(input: $shop) {\\n    content\\n  }\\n}\",\"variables\":{\"shop\":{\"name\":\"Mon shop de test\",\"categoryId\":2,\"countryId\":752,\"city\":\"Paris\",\"url\":\"http://wordpress.dev.dropshifty.com\",\"wcApiUrl\":\"http://wordpress.dev.dropshifty.com\",\"postalCode\":75013,\"description\":\"Description du shop de test qui peut aussi être null\",\"addressLine1\":\"6 rue de la meute\",\"addressLine2\":null,\"picturePath\":null}},\"operationName\":\"NewShop\"}";
-
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-          console.log(this.responseText);
-        }
-      });
-
-      xhr.open("POST", "https://ds-api2.herokuapp.com/");
-      xhr.setRequestHeader("cookie", "PHPSESSID=004a4e235e607f64b9080c94d91477d9");
-      xhr.setRequestHeader("content-type", "application/json");
-
-      xhr.send(data);
+        axios.post("https://ds-api2.herokuapp.com/", {
+          query: `mutation NewShop($shop: ShopInput!) {
+            NewShop(input: $shop) {
+              content
+            }
+          }`,
+          variables: {
+            "shop": {
+              name: "Mon shop de test shopbroo 11",
+              categoryId: 2,
+              countryId: 752,
+              city: "Paris",
+              url: "https://shopbroo.com",
+              wcApiUrl: "https://shopbroo.com",
+              postalCode: 75013,
+              description: "Description du shop de test qui peut aussi être null",
+              addressLine1: "6 rue de la meute",
+              addressLine2: null,
+              picturePath: null
+            }
+          }
+        }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((result) => {
+            console.log(result.data)
+          });
     };
 
     render() {
@@ -53,39 +64,39 @@ class Parameters extends Component {
 
                     <div className="row">
                         <div className="col-12">
-                            <div style={{ width: '70%' }} className="parm_form_1 container mt-4 ml-2">
+                            <div style={{ width: '70%' }} className="mx-auto d-block parm_form_1 container mt-4 ml-2">
                                 <h4>Créer votre boutique China Brand</h4>
 
                                 <div className="mt-3">
                                     <form onSubmit={this.submitParameters}>
                                         <div className="form-group">
                                             <label htmlFor="shop_name">Nom de la boutique</label>
-                                            <input required="required" type="text" name="shop_name" className="_form-control" id="shop_name" placeholder="Nom de la boutique"/>
+                                            <input type="text" name="shop_name" className="_form-control" id="shop_name" placeholder="Nom de la boutique"/>
                                         </div>
 
                                         <div className="form-group">
                                             <label htmlFor="shop_description">Description</label>
-                                            <input required="required" type="text" name="shop_description" className="_form-control" id="shop_description" placeholder="Description"/>
+                                            <input type="text" name="shop_description" className="_form-control" id="shop_description" placeholder="Description"/>
                                         </div>
 
                                         <div className="form-group">
                                             <label htmlFor="shop_city">Ville</label>
-                                            <input required="required" type="text" name="shop_city" className="_form-control" id="shop_city" placeholder="Ville de la boutique"/>
+                                            <input type="text" name="shop_city" className="_form-control" id="shop_city" placeholder="Ville de la boutique"/>
                                         </div>
 
                                         <div className="form-group">
                                             <label htmlFor="shop_address">Adresse</label>
-                                            <input required="required" type="text" name="shop_address" className="_form-control" id="shop_address" placeholder="Ville de la boutique"/>
+                                            <input type="text" name="shop_address" className="_form-control" id="shop_address" placeholder="Ville de la boutique"/>
                                         </div>
 
                                         <div className="form-group">
                                             <label htmlFor="postal_code">Code postal</label>
-                                            <input required="required" type="text" name="postal_code" className="_form-control" id="postal_code" placeholder="Code postal de la boutique"/>
+                                            <input type="text" name="postal_code" className="_form-control" id="postal_code" placeholder="Code postal de la boutique"/>
                                         </div>
 
                                         <div className="form-group">
                                             <label htmlFor="shop_url">Url de la boutique</label>
-                                            <input required="required" type="text" name="shop_url" className="_form-control" id="shop_url" placeholder="Url de la boutique"/>
+                                            <input type="text" name="shop_url" className="_form-control" id="shop_url" placeholder="Url de la boutique"/>
                                         </div>
 
                                         <input type="submit" className="btn-import mt-3" value="Enregistrer" />
@@ -93,11 +104,6 @@ class Parameters extends Component {
                                 </div>
                             </div>
                         </div>
-
-                        <div className="col-6">
-                            {/* Other form */}
-                        </div>
-
                     </div>
 
                 </div>
