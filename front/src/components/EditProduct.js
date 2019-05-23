5ad5571a7bfe9a110ea2203770ac72bbf282b388
julien93140 <julien.moratalla@gmail.com>
2019-05-23 15:46:18 +0200

    edit product in process

--- front/src/components/EditProduct.js
+++ front/src/components/EditProduct.js
@@ -19,30 +19,47 @@ class EditProduct extends Component {
     }
 
     componentDidMount() {
-        let product_id = this.props.match.params.id;
+        //let product_id = this.props.match.params.id;
+        //console.log(product_id)
     }
 
     submitEditProfile = (e) => {
         e.preventDefault();
 
-        let product_id = this.props.match.params.id;
-
-        console.log("edit product form");
-        
+        const id = this.props.match.params.id;
         const title  = e.target.elements.edit_title.value;
         const description = e.target.elements.edit_description.value;
         const price = e.target.elements.edit_price.value;
         const stock = e.target.elements.edit_stock.value;
 
+        var data = '{\"query\":\"{\\n  EditProduct(id:' + '\\\"' + id + '\\\"' + ', name:' + '\\\"' + title + '\\\"' + ', description: ' + '\\\"' + description + '\\\"' + ', price:' + '\\\"' + price + '\\\"' + ', stock: ' + '\\\"' + stock + '\\\"' + ')\\n}\"}';
 
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+        });
+
+        xhr.open("POST", "https://ds-api2.herokuapp.com/");
+        xhr.setRequestHeader("cookie", "PHPSESSID=dbdf85c6a55ebbca573330402391fddc");
+        xhr.setRequestHeader("content-type", "application/json");
+
+        xhr.send(data);
+
+
+        /*
         axios.post("https://ds-api2.herokuapp.com/", {
             query: 
-               `EditProduct(input: $product) {
-                  content
-                }
+               `{
+                    EditProduct(id: " + '\\\"' + sku + '\\\"' + ", name: "caca", description: "un super caca", price: "20.5", stock: 5)
+              }
               `,
               variables: {
                 "product": {
+                    id: id,
                     name: title,
                     description: description,
                     price: price,
@@ -56,6 +73,7 @@ class EditProduct extends Component {
           }).then((result) => {
             console.log(result);
           });
+          */
     }
 
     render() {
@@ -68,8 +86,8 @@ class EditProduct extends Component {
                             <form className="edit_form" onSubmit={this.submitEditProfile}>
                                 <h3>Editer produit</h3>
 
-                                <div className="mt-3"></div>
-                                
+                                <div className="mt-4"></div>
+
                                 <div className="form-group">
                                     <label htmlFor="edit_title">Titre</label>
                                     <input required="required" type="text" name="edit_title" className="_form-control" id="edit_title" placeholder="Titre du produit"/>
@@ -81,13 +99,13 @@ class EditProduct extends Component {
                                 </div>
 
                                 <div className="form-group">
-                                    <label htmlFor="edit_description">Prix</label>
-                                    <input required="required" type="number" name="edit_description" className="_form-control" id="edit_description" placeholder="Prix"/>
+                                    <label htmlFor="edit_price">Prix</label>
+                                    <input required="required" type="number" name="edit_price" className="_form-control" id="edit_price" placeholder="Prix"/>
                                 </div>
 
                                 <div className="form-group">
-                                    <label htmlFor="edit_description">Stock</label>
-                                    <input required="required" type="number" name="edit_description" className="_form-control" id="edit_description" placeholder="Stock"/>
+                                    <label htmlFor="edit_stock">Stock</label>
+                                    <input required="required" type="number" name="edit_stock" className="_form-control" id="edit_stock" placeholder="Stock"/>
                                 </div>
 
                                 <input type="submit" className="btn-import mt-3" value="Editer produit" />
