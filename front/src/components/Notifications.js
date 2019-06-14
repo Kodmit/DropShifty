f2bb6fd7451ebb6db5376a9a051083de0d48e4f9
julien93140 <julien.moratalla@gmail.com>
2019-06-14 18:05:44 +0200

    notifications in process

--- front/src/components/Notifications.js
+++ front/src/components/Notifications.js
@@ -1,59 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/notifications.scss';
+import '../styles/parameters.scss';
 import '../styles/app.scss';
+import '../styles/notifications.scss';
+import axios from "axios";
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/src/sweetalert2.scss';
 import Header from './includes/Header';
 import NavbarSide from './includes/NavbarSide';
+import $ from 'jquery';
+import 'moment';
 
-const Notifications = () => {
-    return (
-        <div className="grid-container">
-            <Header/>
-            <NavbarSide/>
-            <div className="main">
-                <div className="container mt-4">
-                    <h3>Notifications</h3>
-                    <div className="mt-4"></div>
+let moment = require('moment');
 
-                    <div id="notification_1" className="notification-box _shadow mt-4">
-                        <div className="row">
-                            <div className="col-5">
-                                <p>Le prix du produit Iphone X a changé</p>
-                            </div>
-                            <div className="col-6"></div>
-                            <div className="col-1">
-                                <i className="fas fa-times"></i>
-                            </div>
-                        </div>
+const config = require('../components/includes/config.json');
+
+
+class Notifications extends Component {
+
+    constructor(props) {
+        super(props)
+  
+        this.state = {
+          ordersList: [],
+        }
+    }
+  
+      
+    componentDidMount() {
+        this.getOrdersList();
+    }
+
+    getOrdersList() {
+        axios.post(config.config.api_url, {
+            query: `{
+                WC_GetOrdersList
+            }`,
+            }, {
+                headers: {
+                'Content-Type': 'application/json'
+                }
+            }).then((result) => {
+                let res = result.data.data.WC_GetOrdersList;
+
+                this.setState({
+                    ordersList: res
+                });
+            });
+    }
+
+    
+    render() {
+
+        let orders = this.state.ordersList;
+
+        console.log(orders)
+
+        $(orders).each(function(index, value) {
+            if (moment(value.date_created).format('DD/MM/YYYY') == moment().format('DD/MM/YYYY')) {
+                $('.content_notifications').append('<div class="notification mt-3"><p>' + 'Une nouvelle commande passée le ' + moment().format('DD/MM/YYYY') +  '</p></div>')
+            }
+        });
+
+        return (
+            <div className="grid-container">
+                <Header/>
+                <NavbarSide/>
+                <div className="main">
+                    <div className="container">
+                        <h4>Notifications du jour</h4>
                     </div>
 
-                    <div id="notification_2" className="notification-box _shadow mt-4">
-                        <div className="row">
-                            <div className="col-5">
-                                <p>Le produit Changeur n'existe plus</p>
-                            </div>
-                            <div className="col-6"></div>
-                            <div className="col-1">
-                                <i className="fas fa-times"></i>
-                            </div>
-                        </div>
-                    </div>
+                    <div className="content_notifications container mt-4">
 
-                    <div id="notification_3" className="notification-box _shadow mt-4">
-                        <div className="row">
-                            <div className="col-5">
-                                <p>Le prix du produit Film incassable a changé</p>
-                            </div>
-                            <div className="col-6"></div>
-                            <div className="col-1">
-                                <i className="fas fa-times"></i>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
 };
 
 export default Notifications;
